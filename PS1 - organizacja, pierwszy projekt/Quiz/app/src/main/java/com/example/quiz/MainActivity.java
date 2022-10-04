e869f173c9b7f67f1858341242c91b6e420fc8a3
Adam Perkowski <73346398+adpero25@users.noreply.github.com>
2022-10-04 12:09:35 +0200

    Ps1
    Ps1 commit

---
+++ PS1 - organizacja, pierwszy projekt/Quiz/app/src/main/java/com/example/quiz/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button trueButton;
+    private Button falseButton;
+    private Button nextButton;
+    private TextView questionTextView;
+    private TextView scoreTextView;
+    private ProgressBar progressBar;
+    private int score = 0;
+
+
+    private Question[] questions = new Question[] {
+            new Question(R.string.q_activity, true),
+            new Question(R.string.q_find_resources, false),
+            new Question(R.string.q_listener, true),
+            new Question(R.string.q_resources, true),
+            new Question(R.string.q_version, false),
+    };
+
+    int currentIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        trueButton = findViewById(R.id.trueButton);
+        falseButton = findViewById(R.id.falseButton);
+        nextButton = findViewById(R.id.nextButton);
+        questionTextView = findViewById(R.id.question_Text_View);
+        scoreTextView = findViewById(R.id.score_Text_View);
+        progressBar = findViewById(R.id.progressBar);
+
+        progressBar.setVisibility(View.VISIBLE);
+        updateProgressBar();
+
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                checkAnswerCorrectness(true);
+            }
+        });
+
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswerCorrectness(false);
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentIndex = (currentIndex + 1) % questions.length;
+                setNextQuestion();
+                updateProgressBar();
+            }
+        });
+        setNextQuestion();
+    }
+
+    private void setNextQuestion() {
+        questionTextView.setText(questions[currentIndex].getQuestionId());
+    }
+
+    private void updateProgressBar() {
+        progressBar.incrementProgressBy( (100 / questions.length) );
+    }
+
+
+    private void updateScorePoints() {
+        scoreTextView.setText("Your score is " + score);
+    }
+
+
+
+    private void checkAnswerCorrectness(boolean userAnswer) {
+        boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+        int resultMessageId = 0;
+
+        if(userAnswer == correctAnswer){
+            resultMessageId = R.string.correctAnswer;
+            score = score + 1;
+        }
+        else {
+            resultMessageId = R.string.incorrectAnswer;
+        }
+
+        Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
+        updateScorePoints();
+    }
+}
---
+++ PS1 - organizacja, pierwszy projekt/Quiz/app/src/main/java/com/example/quiz/NextActivity.java
@@ -0,0 +1,7 @@
+package com.example.quiz;
+
+import android.app.Activity;
+
+public class NextActivity extends Activity {
+
+}
